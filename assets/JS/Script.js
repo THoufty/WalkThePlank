f003e6f8648826def7befb740445aaa2a2c03ad8
Derb Mane <austinlustik1313@gmail.com>
2022-12-15 21:17:59 -0500

    added js timer

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,2 +1,19 @@
 // JS file
 // git push -u origin feature/apis
+
+//timer
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 15;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left until loss of turn.";
+}
+
+setTime();
