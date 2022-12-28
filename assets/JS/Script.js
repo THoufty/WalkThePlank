83256392523a24871bee0774ed89d69c5a81a5c9
Tom <thomasjhouf@gmail.com>
2022-12-27 21:33:43 -0500

    worked on it a little

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,60 +1,62 @@
 var requestUrl = 'http://api.fungenerators.com/pirate/generate/name?variation=male&limit=1&api_key=xVnLMxZTVVNHCfilqpljlAeF'
-
+var startGame = document.getElementById("fetch-button");
+var usefulBtn = document.getElementById("UsefulButton")
+var turnsLeft = 1
 var fetched = ''
 var timeEl = document.getElementById("time")
-
+// Get the modal
+var infoModal = document.getElementById("infoModal");
+// Get the button that opens the modal
+var infoBtn = document.getElementById("infoButton");
+// Get the <span> element that closes the modal
+var infoSpan = document.getElementsByClassName("infoClose")[0];
+// Get the modal
+var PAModal = document.getElementById("PAModal");
+// Get the button that opens the modal
+var PABtn = document.getElementById("PAButton");
+// Get the <span> element that closes the modal
+var PASpan = document.getElementsByClassName("PAClose2")[0];
 
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-  fetched = data.contents.names
-  console.log(data.contents.names)
+    fetched = data.contents.names
+    console.log(data.contents.names)
 
-  // whatever is going to be using this needs to be called from in here
-  // use "fetched"
+    // whatever is going to be using this needs to be called from in here
+    // use "fetched"
 
   });
 
-  var startGame = document.getElementById("fetch-button");
+startGame.addEventListener('click', function () {
+  // Sets interval in variable
+  var secondsLeft = 15
+  var timerInterval = setInterval(function () {
 
-  startGame.addEventListener('click', function() {
-      // Sets interval in variable
-      var secondsLeft = 15
-      var timerInterval = setInterval(function() {
-      
-        secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left until loss of turn.";
-    
-        if(secondsLeft === 0) {
-          // Stops execution of action at set interval
-          clearInterval(timerInterval);}
-      
-      }, 1000);
-    })
-    
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left until loss of turn.";
+
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+})
 
 ////
 //// Game Information Modal
 ////
 
-// Get the modal
-var infoModal = document.getElementById("infoModal");
-
-// Get the button that opens the modal
-var infoBtn = document.getElementById("infoButton");
-
-// Get the <span> element that closes the modal
-var infoSpan = document.getElementsByClassName("infoClose")[0];
-
 // When the user clicks the button, open the modal 
-infoBtn.onclick = function() {
+infoBtn.onclick = function () {
   infoModal.style.display = "block";
 }
 
 // When the user clicks on <span>, close the modal
-infoSpan.onclick = function() {
+infoSpan.onclick = function () {
   infoModal.style.display = "none";
 }
 
@@ -62,22 +64,13 @@ infoSpan.onclick = function() {
 /////// Play Again Modal
 ///////
 
-// Get the modal
-var PAModal = document.getElementById("PAModal");
-
-// Get the button that opens the modal
-var PABtn = document.getElementById("PAButton");
-
-// Get the <span> element that closes the modal
-var PASpan = document.getElementsByClassName("PAClose2")[0];
-
 // When triggered, open the modal 
-PABtn.onclick = function() {
+PABtn.onclick = function () {
   PAModal.style.display = "block";
 }
 
 // When the user clicks on <span>, close the modal
-PASpan.onclick = function() {
+PASpan.onclick = function () {
   PAModal.style.display = "none";
 }
 
@@ -90,7 +83,7 @@ PASpan.onclick = function() {
 
 // function sweepSprite() {
 //   var turnsLeft = 1
-    
+
 //     if ( turnsLeft == 1 ) {
 //       Element.classList.add("visible")
 //       Element.classList.remove("hidden")
@@ -101,32 +94,31 @@ PASpan.onclick = function() {
 //     }
 //   }
 
-  var usefulBtn = document.getElementById("usefulButton")
-
 // When triggered, change turnsLeft to 1
-usefulBtn.onclick = function() {
+
+usefulBtn.onclick = function () {
   if (turnsLeft == 1) {
-    turnsLeft = 0
+    turnsLeft = 0;
   }
-  else if (turnsLeft == 0) {
-    turnsLeft = 1
+  else if (turnsLeft !== 1) {
+    turnsLeft = 1;
   }
 }
 
+console.log(turnsLeft)
+
 function sweepSprite() {
-  var turnsLeft =1
 
   $(".sprite").each(function () {
-  
 
     if (turnsLeft == 1) {
-      $(this).removeClass("hidden")
-      $(this).addClass("visible")
-     }
-    else if (turnsLeft != 1) {
-      $(this).addClass("hidden")
-      $(this).removeClass("visible")
+      $(".sprite").removeClass("hidden")
+      $(".sprite").addClass("visible")
     }
+    else if (turnsLeft !== 1) {
+      $(".sprite").addClass("hidden")
+      $(".sprite").removeClass("visible")
     }
+  }
   )
 }
